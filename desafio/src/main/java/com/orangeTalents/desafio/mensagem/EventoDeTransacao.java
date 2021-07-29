3e928217d117b6c650a21499140c9f8896982f64
Jess <jessica_yara@yahoo.com.br>
2021-07-29 10:34:16 -0300

    Transações-do-cartão

---
+++ desafio/src/main/java/com/orangeTalents/desafio/mensagem/EventoDeTransacao.java
@@ -0,0 +1,17 @@
+package com.orangeTalents.desafio.mensagem;
+
+public class EventoDeTransacao {
+
+	private String id;
+	
+	private String email;
+
+	public String getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+}
